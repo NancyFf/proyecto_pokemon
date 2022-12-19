cf6035f27dcf77d28d8a72c455bded661c078d53
NancyFf <100386846+NancyFf@users.noreply.github.com>
2022-12-19 14:31:20 -0600

    Add files via upload

---
+++ prueba.sql
@@ -0,0 +1,91 @@
+-- MariaDB dump 10.19  Distrib 10.6.11-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: prueba
+-- ------------------------------------------------------
+-- Server version	10.6.11-MariaDB-0ubuntu0.22.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Pokemon`
+--
+
+DROP TABLE IF EXISTS `Pokemon`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Pokemon` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(30) NOT NULL,
+  `Type` varchar(30) NOT NULL,
+  `Category` varchar(35) NOT NULL,
+  `Weakness` varchar(30) NOT NULL,
+  `Height` decimal(3,2) NOT NULL,
+  `Weight` decimal(3,2) NOT NULL,
+  `Created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `Updated` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `Active` char(1) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Pokemon`
+--
+
+LOCK TABLES `Pokemon` WRITE;
+/*!40000 ALTER TABLE `Pokemon` DISABLE KEYS */;
+INSERT INTO `Pokemon` VALUES (1,'Pikachu','Eléctrico','Ratón','Tierra',0.40,6.00,'2022-11-17 20:55:03','2022-11-17 15:30:40','S');
+/*!40000 ALTER TABLE `Pokemon` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuarios` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Usuario` varchar(255) NOT NULL,
+  `Nombre` varchar(255) NOT NULL,
+  `Apellidos` varchar(255) NOT NULL,
+  `Edad` int(3) NOT NULL,
+  `Genero` char(1) DEFAULT NULL,
+  `Contrasena` varchar(255) NOT NULL,
+  `Fecha_Nacimiento` date DEFAULT NULL,
+  `Activo` char(1) NOT NULL,
+  `Creado` timestamp NOT NULL DEFAULT current_timestamp(),
+  `Modificado` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-12-19 12:49:34
